4177a64296ab1efadaf86c91b5132e2d0f0db329
eslam <abkarino.em2012@gmail.com>
2016-03-09 16:52:40 +0200

    Update test cases

--- CGAlgorithmsUnitTest/src/test/java/cg/algorithmsUnitTest/ExtremePointsTest.java
+++ CGAlgorithmsUnitTest/src/test/java/cg/algorithmsUnitTest/ExtremePointsTest.java
@@ -1,7 +1,6 @@
 package cg.algorithmsUnitTest;
 
 import org.junit.Before;
-import org.junit.Test;
 
 import cg.algorithms.convexHull.ExtremePoints;
 
@@ -11,82 +10,4 @@ public class ExtremePointsTest extends ConvexHullTest {
 	public void initObject() {
 		convexHullTester = new ExtremePoints();
 	}
-
-	@Test
-	public void ExtremePointsTestCase1() {
-
-		Case1();
-	}
-
-	@Test
-	public void ExtremePointsTestCase2() {
-
-		Case2();
-	}
-
-	@Test
-	public void ExtremePointsTestCase3() {
-
-		Case3();
-	}
-
-	@Test
-	public void ExtremePointsTestCase4() {
-
-		Case4();
-	}
-
-	@Test
-	public void ExtremePointsTestCase5() {
-
-		Case5();
-	}
-
-	@Test
-	public void ExtremePointsTestCase6() {
-
-		Case6();
-	}
-
-	@Test
-	public void ExtremePointsTestCase7() {
-
-		Case7();
-	}
-
-	@Test
-	public void ExtremePointsTestCase8() {
-
-		Case8();
-	}
-
-	@Test
-	public void ExtremePointsTestCase9() {
-
-		Case9();
-	}
-
-	@Test
-	public void ExtremePointsTestCase10() {
-
-		Case10();
-	}
-
-	@Test
-	public void ExtremePointsTestCase11() {
-
-		Case11();
-	}
-
-	@Test
-	public void ExtremePointsTestCase12() {
-
-		Case12();
-	}
-
-	@Test
-	public void ExtremePointsTestCase13() {
-		Case13();
-	}
-
 }
--- CGAlgorithmsUnitTest/src/test/java/cg/algorithmsUnitTest/GrahamScanTest.java
+++ CGAlgorithmsUnitTest/src/test/java/cg/algorithmsUnitTest/GrahamScanTest.java
@@ -1,7 +1,6 @@
 package cg.algorithmsUnitTest;
 
 import org.junit.Before;
-import org.junit.Test;
 import cg.algorithms.convexHull.GrahamScan;
 
 public class GrahamScanTest extends ConvexHullTest {
@@ -9,83 +8,5 @@ public class GrahamScanTest extends ConvexHullTest {
 	@Before
 	public void initObject() {
 		convexHullTester = new GrahamScan();
-	}
-
-	@Test
-	public void GrahamScanTestCase1() {
-
-		Case1();
-	}
-
-	@Test
-	public void GrahamScanTestCase2() {
-
-		Case2();
-	}
-
-	@Test
-	public void GrahamScanTestCase3() {
-
-		Case3();
-	}
-
-	@Test
-	public void GrahamScanTestCase4() {
-
-		Case4();
-	}
-
-	@Test
-	public void GrahamScanTestCase5() {
-
-		Case5();
-	}
-
-	@Test
-	public void GrahamScanTestCase6() {
-
-		Case6();
-	}
-
-	@Test
-	public void GrahamScanTestCase7() {
-
-		Case7();
-	}
-
-	@Test
-	public void GrahamScanTestCase8() {
-
-		Case8();
-	}
-
-	@Test
-	public void GrahamScanTestCase9() {
-
-		Case9();
-	}
-
-	@Test
-	public void GrahamScanTestCase10() {
-
-		Case10();
-	}
-
-	@Test
-	public void GrahamScanTestCase11() {
-
-		Case11();
-	}
-
-	@Test
-	public void GrahamScanTestCase12() {
-
-		Case12();
-	}
-
-	@Test
-	public void GrahamScanTestCase13() {
-
-		Case13();
 	}
 }
